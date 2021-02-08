c4bb6387c4540e9c6926070e9361795b6e064ad5
expert-dev1 <odev687@gmail.com>
2021-02-08 12:02:46 +0530

    Commercial Contacts Updated to Same as Residential

--- src/app/commercial-contact/commercial-contact.module.ts
+++ src/app/commercial-contact/commercial-contact.module.ts
@@ -6,16 +6,68 @@ import {
   FormioResource,
   FormioResourceConfig,
   FormioResourceRoutes,
-  FormioResourceService
+  FormioResourceService,
+  FormioResourceIndexComponent,
+  FormioResourceViewComponent,
+  FormioResourceCreateComponent,
+  FormioResourceEditComponent,
+  FormioResourceDeleteComponent,
+  FormioResourceComponent
 } from 'angular-formio/resource';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormioResource,
-    RouterModule.forChild(FormioResourceRoutes())
+    // RouterModule.forChild(FormioResourceRoutes())
+    RouterModule.forChild([
+      {
+        path: '',
+        component: FormioResourceIndexComponent
+      },
+      {
+        path: 'new',
+        component: FormioResourceCreateComponent
+      },
+      {
+        path: ':id',
+        component: FormioResourceComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: 'view',
+            pathMatch: 'full'
+          },
+          {
+            path: 'view',
+            component: ViewComponent
+          },
+          {
+            path: 'edit',
+            component: FormioResourceEditComponent
+          },
+          {
+            path: 'delete',
+            component: FormioResourceDeleteComponent
+          },
+          // {
+          //   path: 'residential-notes',
+          //   loadChildren: './residential-notes/residential-notes.module#ResidentialNotesModule'
+          // },
+          // {
+          //   path: 'residential-leads',
+          //   loadChildren: './residential-leads/residential-leads.module#ResidentialLeadsModule'
+          // },
+          // {
+          //   path: 'residential-viewings',
+          //   loadChildren: './residential-viewings/residential-viewings.module#ResidentialViewingsModule'
+          // }
+        ]
+      }
+    ])
   ],
-  declarations: [],
+  declarations: [ViewComponent],
   providers: [
     FormioResourceService,
     {provide: FormioResourceConfig, useValue: {
---
+++ src/app/commercial-contact/view/view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormioResourceViewComponent } from 'angular-formio/resource';
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.scss']
+})
+export class ViewComponent extends FormioResourceViewComponent implements OnInit  {
+
+
+  ngOnInit() {
+  }
+
+}
