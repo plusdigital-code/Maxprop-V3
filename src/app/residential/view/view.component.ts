77ab6f66696211e34d2f285cb9d79c130a1ce661
expert-dev1 <odev687@gmail.com>
2021-02-10 11:42:12 +0530

    added map tab

--- src/app/residential/view/view.component.ts
+++ src/app/residential/view/view.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { FormioResourceViewComponent } from 'angular-formio/resource';
-
+declare var google; 
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.scss']
 })
 export class ViewComponent extends FormioResourceViewComponent {
+
+
   ngOnInit() {
     window.scrollTo(0, 0);
+    
+    
+  }
+  ngAfterViewInit(){
+    setTimeout(() => {
+      const pointer = this.service.resource.data.address.geometry.location;
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: pointer,
+        zoom: 10,
+      });
+      const marker = new google.maps.Marker({
+        position: pointer,
+        map: map,
+      });
+    },1000)
   }
 }
